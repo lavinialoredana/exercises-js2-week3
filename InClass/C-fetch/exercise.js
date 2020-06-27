828cccf40ee446787f042dae375167497cf51df0
Lavinia Loredana <lavinialoredana@Lavinias-MacBook-Pro.local>
2020-06-27 14:26:01 +0200

    weather app fetch

--- InClass/C-fetch/exercise.js
+++ InClass/C-fetch/exercise.js
@@ -10,3 +10,44 @@ When you get the response from the server, print the current temperature in an <
 
 ================
 */
+// You can construct the URL to fetch from like this. The lat and lon values will come from the input value.
+
+var getButton = document.getElementById("saveButton");
+getButton.addEventListener("click", getWeatherData);
+
+const getWeatherData = (event) => {
+  var latitudeInput = document.querySelector(".latitude");
+  var latitude = latitudeInput.value;
+
+  var longitudeInput = document.querySelector(".longitude");
+  var longitude = longitudeInput.value;
+
+  fetchWeatherData(latitude, longitude);
+  event.preventDefault();
+};
+
+const fetchWeatherData = (latitude, longitude) => {
+  var url = generateWeatherUrl(latitude, longitude);
+
+  fetch(url)
+    .then((resp) => resp.json())
+
+    .then(function (dataJson) {
+      var temp = dataJson.main.temp;
+      showTemperature(temp);
+    });
+};
+
+const generateWeatherUrl = (latitude, longitude) => {
+  var url = "https://fcc-weather-api.glitch.me/api/current";
+  url += "?lat" + latitude;
+  url += "?lon" + longitude;
+  return url;
+};
+
+const showTemperature = (temperature) => {
+  var mainDiv = document.querySelector("#main");
+  var tempHeader = document.createElement("h3");
+  mainDiv.appendChild(tempHeader);
+  tempHeader.innerText = temperature;
+};
